50c384480afe2e5cedf4605c14428c8eb6a381f3
Itthithee <itth@Itthithees-MacBook-Pro.local>
2019-11-22 11:40:53 +0700

    edited

--- src/page/patient.js
+++ src/page/patient.js
@@ -5,24 +5,39 @@ import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap
 import axios from 'axios';
 const patient = () => {
     
+    
     const [searchBy, setSearchBy] = useState('');
     const [word, setWord] = useState('');
-    const [data,setData] = useState({Pateint:[]});
-
+    const [data,setData] = useState([]);
+    let url;
     //fetch data from database เริ่มแก้ตาม tutorial ที่ส่งให้
-    useEffect(()=>{
+    /*{data.map((item)=> <CardPatient 
+        Fname={item.Fname} 
+        Lname={item.Lname} 
+        Gender={item.Gender} 
+        PatientID={item.PatientID} 
+        Tel={item.Tel} 
+        CousinTel={item.CounsinTel} 
+        Address={item.Address}/>)}*/
+    useEffect(async ()=>{
         if (searchBy == 'name'){
-            const url
-        }else if(serchBy == 'ID'){
-            const url
+             url =  'http://localhost:5000/patient/findbyname/:fname/:lname'
+        }else if(searchBy == 'ID'){
+             url = 'http://localhost:5000/patient/findbyid/:id'
         }else{
-            const url 
-        ]
-        axios.get(url)
-        .then(res=>{
-            setData({res})
+             url = 'http://localhost:5000/patient'
+        }
+        //let result = await 
+        axios.get(url).then((res)=>{
+        const json = res.data
+        console.log(json);
+        setData(json);
+        console.log(data);
         })
-    });
+        
+            
+    },[]);
+    
     
     ///////////////////////////////////////////////////
     return (
@@ -43,19 +58,20 @@ const patient = () => {
                         <input type="text" class="form-control" aria-label="Text input with dropdown button" />
                         <button type="submit" class="btn btn-primary" >ยืนยัน</button>
                     </div>
-
+                
                 </FormGroup>
             </Form>
-
-           {data.map((item)=> <CardPatient 
-           Fname={item.Fname} 
-           Lname={item.Lname} 
-           Gender={item.Gender} 
-           PatientID={item.PatientID} 
-           Tel={item.Tel} 
-           CousinTel={item.CounsinTel} 
-           Address={item.Address}/>)}
+            {data.map((item)=> <CardPatient 
+        Fname={item.Fname} 
+        Lname={item.Lname} 
+        Gender={item.Gender} 
+        PatientID={item.PatientID} 
+        Tel={item.Tel} 
+        CousinTel={item.CounsinTel} 
+        Address={item.Address}/>)}
+            
         </div>
+        
     );
 };
 
